63703e74320cca50130979dd69f8662e223621e8
Roland Müller <roland-georg.mueller@senacor.com>
2018-09-28 18:01:26 +0200

    Task 2.4: Return 404 for not existing channels.

--- services/src/main/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageService.kt
+++ services/src/main/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageService.kt
@@ -1,10 +1,11 @@
 package com.senacor.code.fullstack.chat.service
 
 import com.senacor.code.fullstack.chat.domain.ChatMessage
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ChatMessageService {
+class ChatMessageService(@Autowired private var channelService: ChannelService) {
 
     companion object {
         val messages = listOf(
@@ -13,6 +14,9 @@ class ChatMessageService {
     }
 
     fun loadChatMessages(channelId: String): List<ChatMessage> {
+        if(!channelService.existsChannel(channelId)) {
+            throw ChannelNotFoundException()
+        }
         return messages
     }
 }
--- services/src/test/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageServiceTest.kt
+++ services/src/test/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageServiceTest.kt
@@ -1,17 +1,29 @@
 package com.senacor.code.fullstack.chat.service
 
 import com.senacor.code.fullstack.chat.domain.ChatMessage
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ChatMessageServiceTest {
-    private var service = ChatMessageService()
+    private var channelServiceMock = mockk<ChannelService>()
+    private var service = ChatMessageService(channelServiceMock)
 
     @Test fun fetchChatMessages() {
-        var result = service.loadChatMessages("dev")
+        every { channelServiceMock.existsChannel("dev") } returns true
+        val result = service.loadChatMessages("dev")
 
         assertEquals(2, result.size)
         assertEquals("Hello", result[0].message)
         assertEquals("World!", result[1].message)
+    }
+
+    @Test(expected = ChannelNotFoundException::class)
+    @Throws(ChannelNotFoundException::class)
+    fun loadChatMessagesThrowsExceptionIfChannelNotExist() {
+        every { channelServiceMock.existsChannel("not-a-channel") } returns false
+
+        service.loadChatMessages("not-a-channel")
     }
 }
