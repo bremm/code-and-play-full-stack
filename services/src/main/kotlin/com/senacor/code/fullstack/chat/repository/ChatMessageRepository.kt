e99d47d4268bf4a5accf3d7f2301dbbd88ba2b86
Roland Müller <roland-georg.mueller@senacor.com>
2018-10-01 15:51:58 +0200

    Task 3.2: Read messages from the database.

--- services/src/main/kotlin/com/senacor/code/fullstack/chat/repository/ChatMessageRepository.kt
+++ services/src/main/kotlin/com/senacor/code/fullstack/chat/repository/ChatMessageRepository.kt
@@ -3,4 +3,6 @@ package com.senacor.code.fullstack.chat.repository
 import com.senacor.code.fullstack.chat.domain.ChatMessage
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface ChatMessageRepository : MongoRepository<ChatMessage, String>
+interface ChatMessageRepository : MongoRepository<ChatMessage, String> {
+    fun findByChannelIdOrderByCreationTimestampAsc(channelId: String): List<ChatMessage>
+}
