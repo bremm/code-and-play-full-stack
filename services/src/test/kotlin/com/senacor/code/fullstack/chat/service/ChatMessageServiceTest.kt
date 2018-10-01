296a562571a820493c991a2fe479fb4f04a9fa5c
Roland Müller <roland-georg.mueller@senacor.com>
2018-10-01 16:49:26 +0200

    Task 4.1: A service to store messages.

--- services/src/test/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageServiceTest.kt
+++ services/src/test/kotlin/com/senacor/code/fullstack/chat/service/ChatMessageServiceTest.kt
@@ -4,8 +4,13 @@ import com.senacor.code.fullstack.chat.domain.ChatMessage
 import com.senacor.code.fullstack.chat.repository.ChatMessageRepository
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import io.mockk.verifySequence
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.time.Duration
+import java.time.Instant
 
 class ChatMessageServiceTest {
     private var channelServiceMock = mockk<ChannelService>()
@@ -33,5 +38,36 @@ class ChatMessageServiceTest {
         every { channelServiceMock.existsChannel("not-a-channel") } returns false
 
         chatMessageService.loadChatMessages("not-a-channel")
+    }
+
+    @Test
+    @Throws(ChannelNotFoundException::class)
+    fun saveChatMessage() {
+        every { channelServiceMock.existsChannel("dev") } returns true
+        every {
+            chatMessageRepository.save(any<ChatMessage>())
+        } answers {
+            this.value.id = "has-an-id"
+            this.value
+        }
+
+        val savedMessage = chatMessageService.saveChatMessage("dev", "sender@test.de", "Hello World!")
+
+        verify { chatMessageRepository.save(any<ChatMessage>()) }
+
+        assertEquals("dev", savedMessage.channelId)
+        assertEquals("sender@test.de", savedMessage.sender)
+        assertEquals("Hello World!", savedMessage.message)
+        assertEquals("has-an-id", savedMessage.id)
+        assertTrue(Duration.between(savedMessage.creationTimestamp, Instant.now()).abs().seconds < 3)
+    }
+
+    @Test(expected = ChannelNotFoundException::class)
+    @Throws(ChannelNotFoundException::class)
+    fun createChatMessagesThrowsExceptionIfChannelNotExist() {
+
+        every { channelServiceMock.existsChannel("not-a-channel") } returns false
+
+        chatMessageService.saveChatMessage("not-a-channel", "sender@test.de", "Hello World!")
     }
 }
